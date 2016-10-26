e2e9d8c9c6d3992e7eb7765ccf0f48a0db09fbcc
BEKHTIKarim <b.bekti.karim@gmail.com>
2016-10-26 13:48:45 +0200

    Amélioration de voila quoi

---
+++ container_admin.php
@@ -0,0 +1,32 @@
+<?php include('get_all_books.php'); ?>
+<div class="container main">
+
+
+  <div class="table-responsive">
+    <table class="table table-hover">
+
+      <thead>
+        <tr>
+           <th>#</th>
+           <th>Titre</th>
+           <th>Auteur</th>
+           <th>Date de publication</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($books as $book) :?>
+          <tr>
+            <td> <?php print $book['id']?> </td>
+            <td> <?php print $book['title']?></td>
+            <td> <?php print $book['author']?></td>
+            <td> <?php print date('Y', $book['publish_date'])?></td>
+          </tr>
+        <?php endforeach ?>
+
+      </tbody>
+
+    </table>
+  </div>
+
+
+</div>
